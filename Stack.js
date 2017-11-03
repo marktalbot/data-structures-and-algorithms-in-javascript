567127dc9e006dd932d5d6d590877418c48290c7
Mark Talbot <mark.talbot@experiencepoint.com>
2017-11-03 13:42:43 -0400

    Subbing out Stack class

---
+++ Stack.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// See exercise here:
+// https://github.com/kuychaco/algoClass/blob/master/data-structures/stack.js
+
+class Stack {
+    constructor(capacity) {
+        this.capacity = capacity;
+    }
+
+    push() {}
+
+    pop() {}
+
+    peek() {}
+
+    count() {}
+}
+
+module.exports = Stack;
